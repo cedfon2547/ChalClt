7eebfb6c3833b2d78fff8d3c39d49214cd05a923
Cedric B. Marsolais <cedric-b.marsolais.1@ulaval.ca>
2023-11-01 14:07:05 -0400

    first commit test

--- chalCLT/src/main/java/ca/ulaval/glo2004/App.java
+++ chalCLT/src/main/java/ca/ulaval/glo2004/App.java
@@ -7,11 +7,7 @@ import ca.ulaval.glo2004.gui.components.MainWindowTopBarMenu;
 import javax.swing.*;
 
 public class App {
-    // Exemple de creation d'une fenetre et d'un bouton avec swing. Lorsque vous
-    // allez creer votre propre GUI
-    // Vous n'aurez pas besoin d'ecrire tout ce code, il sera genere automatiquement
-    // par intellij ou netbeans
-    // Par contre vous aurez a creer les actions listener pour vos boutons et etc.
+
     public static void main(String[] args) {
         Controleur controleur = Controleur.getInstance();
 
