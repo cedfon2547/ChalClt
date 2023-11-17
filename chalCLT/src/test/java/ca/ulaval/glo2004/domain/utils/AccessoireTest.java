42479e436f5fe93c711b0f0b69f0afb7e5261eba
Cédric-B. Marsolais <cedric-b.marsolais.1@ulaval.ca>
2023-11-16 20:55:03 -0500

    Ajout de test pour AccessoireTest RetraitTest

---
+++ chalCLT/src/test/java/ca/ulaval/glo2004/domain/utils/AccessoireTest.java
@@ -0,0 +1,84 @@
+package ca.ulaval.glo2004.domain.utils;
+
+import ca.ulaval.glo2004.domaine.Accessoire;
+import ca.ulaval.glo2004.domaine.TypeAccessoire;
+import ca.ulaval.glo2004.domaine.TypeMur;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AccessoireTest {
+
+    @Test
+    public void InstanceTestFenetre(){
+        TypeAccessoire typeAccessoire = TypeAccessoire.Fenetre;
+        TypeMur typeMur = TypeMur.Droit;
+        double[] pos = {2.0, 3.0};
+        double[] dim = {1.0,2.0};
+
+        Accessoire accessoire = new Accessoire(typeAccessoire, typeMur,pos, dim );
+
+        assertEquals(TypeAccessoire.Fenetre, accessoire.getAccessoireType());
+        assertEquals(TypeMur.Droit, accessoire.getTypeMur());
+        assertEquals(2, accessoire.getPosition()[0], 0);
+        assertEquals(3, accessoire.getPosition()[1], 0);
+        assertEquals(1, accessoire.getDimension()[0], 0);
+        assertEquals(2, accessoire.getDimension()[1], 0);
+    }
+
+    @Test
+    public void InstanceTestPorte(){
+        TypeAccessoire typeAccessoire = TypeAccessoire.Porte;
+        TypeMur typeMur = TypeMur.Droit;
+        double[] pos = {2.0, 3.0};
+        double[] dim = {1.0,2.0};
+
+        Accessoire accessoire = new Accessoire(typeAccessoire, typeMur,pos, dim );
+
+        assertEquals(TypeAccessoire.Porte, accessoire.getAccessoireType());
+        assertEquals(TypeMur.Droit, accessoire.getTypeMur());
+        assertEquals(2, accessoire.getPosition()[0], 0);
+        assertEquals(3, accessoire.getPosition()[1], 0);
+        assertEquals(1, accessoire.getDimension()[0], 0);
+        assertEquals(2, accessoire.getDimension()[1], 0);
+    }
+
+    @Test
+    public void SetTest(){
+        TypeAccessoire typeAccessoire = TypeAccessoire.Porte;
+        TypeMur typeMur = TypeMur.Droit;
+        double[] pos = {4.0, 3.0};
+        double[] dim = {7.0,2.0};
+        UUID uuid = UUID.randomUUID();
+        String nom = "NomRandom101";
+        Accessoire accessoire = new Accessoire(typeAccessoire, typeMur,pos, dim);
+
+        accessoire.setAccessoireType(TypeAccessoire.Fenetre);
+        assertEquals(TypeAccessoire.Fenetre, accessoire.getAccessoireType());
+
+        accessoire.setAccessoireId(uuid);
+        assertEquals(uuid, accessoire.getAccessoireId());
+
+        accessoire.setAccessoireNom(nom);
+        assertEquals(nom, accessoire.getAccessoireNom());
+
+        accessoire.setValide(true);
+        assertTrue(accessoire.getValide());
+    }
+
+    @Test
+    public void getMarginWithRectTest(){
+        TypeAccessoire typeAccessoire = TypeAccessoire.Porte;
+        TypeMur typeMur = TypeMur.Droit;
+        double[] pos = {4.0, 3.0};
+        double[] dim = {7.0,2.0};
+        Accessoire accessoire = new Accessoire(typeAccessoire, typeMur,pos, dim);
+
+       // DOTO
+
+    }
+
+}
---
+++ chalCLT/src/test/java/ca/ulaval/glo2004/domain/utils/RetraitTest.java
@@ -0,0 +1,45 @@
+package ca.ulaval.glo2004.domain.utils;
+
+import ca.ulaval.glo2004.domaine.Retrait;
+import ca.ulaval.glo2004.domaine.TypeRetrait;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RetraitTest {
+
+    @Test
+    public void testInstanceAcessoire(){
+        double[] pos = {4.0, 8.0};
+        double[] dim = {3.0, 6.0};
+        Retrait retrait = new Retrait(TypeRetrait.Accessoire, pos , dim);
+
+        assertEquals(TypeRetrait.Accessoire, retrait.getType());
+        assertEquals(4.0, retrait.getPosition()[0], 0);
+        assertEquals(8.0, retrait.getPosition()[1], 0);
+        assertEquals(3.0, retrait.getDimension()[0], 0);
+        assertEquals(6.0, retrait.getDimension()[1], 0);
+    }
+
+    @Test
+    public void testInstanceMur(){
+        double[] pos = {2.0, 3.0};
+        double[] dim = {5.0, 2.0};
+        Retrait retrait = new Retrait(TypeRetrait.Rainure, pos , dim);
+
+        assertEquals(TypeRetrait.Rainure, retrait.getType());
+        assertEquals(2.0, retrait.getPosition()[0], 0);
+        assertEquals(3.0, retrait.getPosition()[1], 0);
+        assertEquals(5.0, retrait.getDimension()[0], 0);
+        assertEquals(2.0, retrait.getDimension()[1], 0);
+    }
+
+    @Test
+    public void testsetType(){
+        double[] pos = {2.0, 3.0};
+        double[] dim = {5.0, 2.0};
+        Retrait retrait = new Retrait(TypeRetrait.Rainure, pos , dim);
+        retrait.setType(TypeRetrait.Accessoire);
+        assertEquals(TypeRetrait.Accessoire, retrait.getType());
+    }
+}
